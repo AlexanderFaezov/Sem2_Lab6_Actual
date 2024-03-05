34c84bb213632647bc7b3cbc3acea0b6625f4029
AlexanderFaezov <s.faezov.com@mail.ru>
2024-03-05 19:12:36 +0500

    Добавьте файлы проекта.

---
+++ Sem2_Lab6_Actual/Sem2_Lab6_Actual.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    string input;
+    cout << "Введите строку: ";
+    getline(std::cin, input);
+
+    istringstream iss(input);
+    string word;
+    while (iss >> word) {
+        reverse(word.begin(), word.end());
+        cout << word << " ";
+    }
+
+    return 0;
+}
